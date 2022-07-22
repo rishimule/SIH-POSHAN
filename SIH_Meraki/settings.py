e8f7a5ec504ea25c0289c76a1c7b94573366715b
Radon Gas <radonin123@yandex.com>
2022-07-22 08:13:26 +0000

    setting tables

--- SIH_Meraki/settings.py
+++ SIH_Meraki/settings.py
@@ -38,12 +38,6 @@ runserver.default_port = '8080'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'accounts',
     'schools',
     'talukas',
@@ -51,6 +45,13 @@ INSTALLED_APPS = [
     'states',
     'national',
     'bootstrap4',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
