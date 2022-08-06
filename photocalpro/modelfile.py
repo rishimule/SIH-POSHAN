c56349007f39d532a612c0217583fb03df1ea1d9
Rishi Mule <radonintro1234@gmail.com>
2022-08-06 11:36:49 +0530

    Temp Function

--- photocalpro/modelfile.py
+++ photocalpro/modelfile.py
@@ -89,6 +89,28 @@ def processed_img(img_path):
 
 
 
+# def return_calories_proteins(image='https://www.vegrecipesofindia.com/wp-content/uploads/2021/02/khichdi-recipe-1.jpg',meal_name='Khicdi'):
+#     """_summary_
+#     Args:
+#         image (_type_): Image of  meal
+#         meal_name (_type_): Name of meal
+#     Returns:
+#         Dict: {proteins, calories}
+#     """
+#     # your code here
+#     result= processed_img(image)
+#     cal = fetch_calories(result)
+#     pro = fetch_proteins(result)
+
+    
+
+#     return {
+#         'proteins' : pro,
+#         'calories' : cal,
+        
+#     }
+
+
 def return_calories_proteins(image='https://www.vegrecipesofindia.com/wp-content/uploads/2021/02/khichdi-recipe-1.jpg',meal_name='Khicdi'):
     """_summary_
     Args:
@@ -98,15 +120,11 @@ def return_calories_proteins(image='https://www.vegrecipesofindia.com/wp-content
         Dict: {proteins, calories}
     """
     # your code here
-    result= processed_img(image)
-    cal = fetch_calories(result)
-    pro = fetch_proteins(result)
-
-    
+ 
 
     return {
-        'proteins' : pro,
-        'calories' : cal,
+        'proteins' : 66,
+        'calories' : 225.6,
         
     }
 
