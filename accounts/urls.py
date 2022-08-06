3d24d3e4d799e6bf2d107eff89c57b903bc90f2e
Rishi Mule <radonintro1234@gmail.com>
2022-08-06 15:34:32 +0530

    Temporary Trial Function

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,30 +5,63 @@ from django.contrib.auth.views import (
     PasswordResetView,
     PasswordResetDoneView,
     PasswordResetConfirmView,
-    PasswordResetCompleteView
+    PasswordResetCompleteView,
+    PasswordChangeDoneView,
+    PasswordChangeView
 )
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('password_reset', PasswordResetView.as_view(
-        template_name='accounts/password_reset.html',
-        email_template_name='accounts/password_reset_email.html',
-        subject_template_name='accounts/password_reset_subject.txt',
-        success_url=reverse_lazy('accounts:password_reset_done')
-    ), name='password_reset'),
-    path('password_reset_done/', PasswordResetDoneView.as_view(
-        template_name='accounts/password_reset_done.html'
-    ), name='password_reset_done'),
-    path('password_reset_<uidb64>_<token>/', PasswordResetConfirmView.as_view(
-        template_name='accounts/password_reset_confirm.html',
-        success_url=reverse_lazy('accounts:password_reset_complete')
-    ), name='password_reset_confirm'),
-    path('password_reset_complete/', PasswordResetCompleteView.as_view(
-        template_name='accounts/password_reset_complete.html'
-    ), name='password_reset_complete'),
-    
+    path('login/',
+         LoginView.as_view(template_name='accounts/login.html',
+                           redirect_authenticated_user=True),
+         name='login'),
+
+    path('logout/',
+         LogoutView.as_view(),
+         name='logout'),
+
+    path('password_change/',
+         PasswordChangeView.as_view(
+             template_name='accounts/password_change_form.html',
+             success_url=reverse_lazy('accounts:password_change_done')
+         ),
+         name='password_change'),
+
+    path('password_change_done/',
+         PasswordChangeDoneView.as_view(
+             template_name='accounts/password_change_done.html'
+         ),
+         name='password_change_done'),
+
+    path('password_reset',
+         PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt',
+             success_url=reverse_lazy('accounts:password_reset_done')
+         ),
+         name='password_reset'),
+
+    path('password_reset_done/',
+         PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('password_reset_<uidb64>_<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html',
+             success_url=reverse_lazy('accounts:password_reset_complete')
+         ),
+         name='password_reset_confirm'),
+
+    path('password_reset_complete/',
+         PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
 ]
---
+++ photocalpro/modelfile2.py
@@ -0,0 +1,16 @@
+def return_calories_proteins(image='https://www.vegrecipesofindia.com/wp-content/uploads/2021/02/khichdi-recipe-1.jpg',meal_name='Khicdi'):
+    """_summary_
+    Args:
+        image (str): Path to Image of  meal
+        meal_name (str): Name of meal
+    Returns:
+        Dict: {proteins, calories}
+    """
+    # your code here
+ 
+
+    return {
+        'proteins' : 66,
+        'calories' : 225.6,
+        
+    }
