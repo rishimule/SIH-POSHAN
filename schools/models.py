4b2cfbffdc0dc64c2c6f4bd81d5acb1c93e572df
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2022-08-25 10:25:35 +0530

    solved it

--- schools/models.py
+++ schools/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from msilib.schema import Property
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.urls import reverse
