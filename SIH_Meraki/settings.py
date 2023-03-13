fa21670d194b624d6fbcbfd7a27f0f107cfb8c90
Rishi Mule <47552820+rishimule@users.noreply.github.com>
2023-03-13 08:15:34 +0530

    little changes

--- SIH_Meraki/settings.py
+++ SIH_Meraki/settings.py
@@ -83,7 +83,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+    'http://localhost:3000'
 ]
     
 ROOT_URLCONF = 'SIH_Meraki.urls'
--- merakiiextras/hostSettings.py
+++ merakiiextras/hostSettings.py
@@ -1,2 +1,2 @@
-default_runserver_address = "0.0.0.0"
+default_runserver_address = "127.0.0.1"
 default_runserver_port = '8080'
